d4262092aab1bb06fe8eda9b60cd3a24ab24d3cc
square-bit <info@square-bit.com>
2019-05-25 17:06:41 +0100

    EVA (short for Email Validator) version One

---
+++ readme.md
@@ -0,0 +1,35 @@
+# EVA (short for Email Validator)
+
+EVA (short for Email Validator) is a validation tool aimed at keeping your platforms free from fake or disposable email accounts.
+
+This plugin for Javascript provides an easy 3 step integration to get you set and running in no time:
+
+1 - Download and install it in your Javascript system
+2 - Create and account at [https://e-va.io](https://e-va.io) and generate an API Key
+3 - Configure this module with the newly generated API KEY.
+
+That's it. Your Javascript system is now able to keep those fake email accounts at bay.
+
+Any questions, issues, suggestions, contact us at [support@e-va.io](mailto:support@e-va.io)
+
+>## Installing
+>EVA email validation and checker JavaScript plugin is easy to install. Copy the code below and paste just before the </body> tag of your website.
+```
+<script type="text/javascript">
+
+EVA_API_KEY = 'YOUR_PUBLIC_API_KEY_HERE';
+
+// In case of a slow connection the validation may take longer than desired.
+// in those cases, the request may timeout.
+// Here you can set that timeout or simply leave it commented to use the default (3 seconds).
+// var EVA_TIMEOUT_SECONDS = 3;
+
+</script>
+
+<script async type="text/javascript" src="cdn_JS"></script>
+```
+
+>## Important Notes:
+>- You can get your API key from EVA dashboard.
+>- Do not forget to add your website address into approved CORS domains of your API key.
+>- The widget will search for all fields with the attribute type="email" and class="eva-validator".
---
+++ src/script.js
@@ -0,0 +1,101 @@
+window.addEventListener('load', function () {
+
+  var evainput = document.getElementsByClassName('eva-validator');
+
+  // add eventlistener to all forms that have an email that needs validation
+  Array.prototype.forEach.call(evainput, function (element, index, array) {
+    getFormFor(element).addEventListener('submit', formSubmitHandler)
+  });
+
+  // add the onBlur event listener for all the email inputs
+  Array.prototype.forEach.call(evainput, function (element, index, array) {
+    element.addEventListener('blur', textInputBlurHandler);
+  });
+
+  function textInputBlurHandler() {
+    var email = this;
+    var frm = getFormFor(this);
+
+    // disable form submit until we validate
+    frm.eva = frm.eva || {}; // get or create the 'eva' parameter
+    frm.eva.valid = false;
+
+    // validate
+    message = makeRequest(email.value, function (result, messageInfo) {
+      // enable, if valid, the form submission
+      frm.eva.valid = result;
+
+      // Remove eva-errors span
+      var errors = this.getElementsByClassName('eva-errors');
+      Array.prototype.forEach.call(errors, function (element, index, array) {
+        element.remove();
+      });
+
+      // invalid add class 'error' to input
+      if (result) {
+        this.classList.remove('eva-error');
+      } else {
+        this.classList.add('eva-error');
+        var errorMessage = document.createElement('span');
+        errorMessage.setAttribute('class', 'eva-errors');
+        errorMessage.innerHTML = "Result: " + messageInfo;
+        email.before(errorMessage);
+      }
+
+    }.bind(frm));
+  }
+
+  function formSubmitHandler(event) {
+    var frm = this;
+
+    if (frm.eva && frm.eva.valid == false) {
+      event.preventDefault();
+    }
+  }
+
+  function makeRequest(email, callback) {
+    if (email) {
+      var url = 'http://eva.drupyons.com/api/email/validate/' + email;
+      var EVA_TIMEOUT_SECONDS = EVA_TIMEOUT_SECONDS || 3;
+
+      let xhr = new XMLHttpRequest();
+      xhr.open('GET', url, true);
+      xhr.setRequestHeader("api-key", EVA_API_KEY);
+      xhr.setRequestHeader("Accept", 'application/json');
+      xhr.responseType = 'json';
+      xhr.timeout = EVA_TIMEOUT_SECONDS * 1000; // time in milliseconds
+      xhr.send();
+
+      // // 4. This will be called after the response is received
+      xhr.onload = function () {
+        if (xhr.status != 200) { // analyze HTTP status of the response
+          callback(false, xhr.response.message);
+        } else { // show the result
+          callback(xhr.response.state != undefined && xhr.response.state == 'Safe', xhr.response.message);
+        }
+      };
+      xhr.ontimeout = function (e) {
+        callback(false, 'EVA API call failed.');
+      };
+      xhr.onerror = function () {
+        if (xhr.response) {
+          callback(false, xhr.response.message);
+        }
+        else {
+          var domain = location.protocol + "://" + window.location.hostname;
+          console.log("WARNING ============");
+          console.log("Make sure the API key('" + EVA_API_KEY + "') is correct and that '" + domain + "' is in the list of allowed domains for this key");
+          console.log("You can do that at https://e-va.io")
+          console.log("Allowing email through..");
+          console.log("====================");
+          callback(true);
+        }
+      }
+    }
+  }
+});
+
+// get the form that owns this element
+function getFormFor(element) {
+  return element.form || element.closest('form');
+}
